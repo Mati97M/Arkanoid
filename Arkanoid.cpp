dea15572d5a70febff9c5f0cc69a64cfb8e59e74
Mateusz Mieckowski / R&D POLAND <mateusz.mieckowski@ige-xao.com.pl>
2022-10-14 23:27:55 +0200

    fixed bug with resizing,  added Resizer and Visible

--- Arkanoid.cpp
+++ Arkanoid.cpp
@@ -6,23 +6,74 @@
 #include <iostream>
 #include <string>
 
-/* Test Framework realization */
+
+class Arkanoid;
+
+class Resizer
+{
+	int m_SreenWidth;
+	int m_ScreenHeight;
+
+	Resizer()
+	{
+		getScreenSize(m_SreenWidth, m_ScreenHeight);
+	}
+	friend class Arkanoid;
+
+public:
+	void Resize(int & a_spriteWidth, int & a_spriteHeight)
+	{
+		double scale{};
+	}
+};
+
+class Visible
+{
+	Sprite* m_Sprite;
+	const char* m_SpritePath;
+protected:
+	int m_Width, m_Height;
+	int m_x, m_y;
+
+	Visible(const char* a_SpritePath, int a_width, int a_height, int x, int y): m_SpritePath{a_SpritePath}, m_Width{a_width}, m_Height{a_height}, m_x{x}, m_y{y}
+	{
+		m_Sprite = createSprite(m_SpritePath);
+		if (!m_Sprite)
+		{
+			std::cout << "Sth went wrong while creating the sprite with a  given path: " << a_SpritePath << std::endl;
+			return;
+		}
+		setSpriteSize(m_Sprite, m_Width, m_Height);
+	}
+	friend class Arkanoid;
+};
+
 class Arkanoid : public Framework {
 	
 	int WIDTH{};
 	int HEIGHT{};
 	bool FULLSCREEN{};
+	Resizer* m_Resizer{};
+	Visible* m_Background{};
 
 public:
 	Arkanoid(int width, int height, bool fullscreen) : WIDTH{ width }, HEIGHT{ height }, FULLSCREEN{fullscreen}, Framework()
 	{
+		m_Resizer = new Resizer();
+		m_Background = new Visible("background.png", WIDTH, HEIGHT, 0, 0);
 
+	}
+	virtual ~Arkanoid()
+	{
+		delete m_Resizer;
+		delete m_Background;
 	}
 
 	virtual void PreInit(int& width, int& height, bool& fullscreen)
 	{
 		if (!FULLSCREEN)				
 		{
+			fullscreen = false;
 			width = WIDTH;
 			height = HEIGHT;
 		}
@@ -40,7 +91,8 @@ public:
 	}
 
 	virtual bool Tick() {
-		drawTestBackground();
+		drawSprite(m_Background->m_Sprite, m_Background->m_x, m_Background->m_y);
+		//drawTestBackground();
 		return false;
 	}
 
@@ -63,20 +115,20 @@ public:
 		return "Arcanoid";
 	}
 };
-struct Dimensions
+struct SreenDimensions
 {
 	int Width;
 	int Height;
 	bool fullscreen{ true };
 };
 
-Dimensions get_sizes(int argc, char* argv[]);
+SreenDimensions get_sizes(int argc, char* argv[]);
 
 
 int main(int argc, char* argv[])
 {
 	//////////////// wymiary okna poczatek
-	Dimensions dimensions{ get_sizes(argc,argv) };
+	SreenDimensions dimensions{ get_sizes(argc,argv) };
 	//////////////////// wymiary okna koniec
 
 	bool fullscreen{};
@@ -93,17 +145,17 @@ int main(int argc, char* argv[])
 }
 
 
-Dimensions get_sizes(int argc, char* argv[])
+SreenDimensions get_sizes(int argc, char* argv[])
 {
 	std::string dimensions = argv[3];
 	size_t separatorPos = dimensions.find_first_of('x');
 	
 	if (separatorPos == std::string::npos)
-		return Dimensions{ 0,0,true };
+		return SreenDimensions{ 0,0,true };
 
 	std::string sWidth{ dimensions.substr(0,separatorPos) };
 	std::string sHeight{ dimensions.substr(separatorPos + 1) };
 	int Width = std::stoi(sWidth);
 	int Height = std::stoi(sHeight);
-	return Dimensions{ Width, Height,false };
+	return SreenDimensions{ Width, Height,false };
 }
